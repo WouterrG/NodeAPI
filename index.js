68c6d21504346b4470f5299302e26b61dde8edac
PROD\wdegruijl <wouter.degruijl@alten.nl>
2023-02-20 13:51:35 +0100

    post functionality works

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-import express from 'express';
+import express from "express";
 import _ from "lodash";
-import mongoose from 'mongoose';
-import Comment from './models/Comment.js';
+import mongoose from "mongoose";
+import Comment from "./models/Comment.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/outfit", (req, res) => {
-    const tops = ["Black", "White", "Orange", "Navy"];
-    const jeans = ["Black", "Beige", "Grey", "Navy"];
-    const shoes = ["Black", "White", "Brown"];
+  const tops = ["Black", "White", "Orange", "Navy"];
+  const jeans = ["Black", "Beige", "Grey", "Navy"];
+  const shoes = ["Black", "White", "Brown"];
 
-    res.json({
-        top: _.sample(tops),
-        jeans: _.sample(jeans),
-        shoes: _.sample(shoes)
-    });
+  res.json({
+    top: _.sample(tops),
+    jeans: _.sample(jeans),
+    shoes: _.sample(shoes),
+  });
 });
 
 app.get("/comment/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log(id)
+  const { id } = req.params;
+  console.log(id);
 
-    try {
-        const comment = await Comment.findById(id);
-        return res.status(200).json(comment);
-
-    } catch (err) {
-        return res.sendStatus(404);
-    }
-})
+  try {
+    const comment = await Comment.findById(id);
+    return res.status(200).json(comment);
+  } catch (err) {
+    return res.sendStatus(404);
+  }
+});
 
 app.post("/comments", async (req, res) => {
-    const content = req.body.content;
+  const content = req.body.content;
 
-    if (!content) {
-        return res.sendStatus(400);
-    }
+  if (!content) {
+    return res.sendStatus(400);
+  }
 
-    const newComment = new Comment({
-        comment: content.comment,
-        user: content.user
-    });
-    await newComment.save()
+  const newComment = new Comment({
+    comment: content.comment,
+    user: content.user,
+  });
+  await newComment.save();
 
-    res.status(201).json({
-        comment: newComment,
-    });
-})
+  res.status(201).json({
+    comment: newComment,
+  });
+});
 
-app.post("/summation", async (req, res) => {  
+app.post("/summation", async (req, res) => {
+  if (!req.body.content) {
+    return res.sendStatus(400);
+  }
 
-    if (!req.body.content) {
-        return res.sendStatus(400);
-    }
+  const firstNumber = req.body.content.firstNumber;
+  const secondNumber = req.body.content.secondNumber;
 
-    const firstNumber = req.body.content.firstNumber;
-    const secondNumber = req.body.content.secondNumber;
+  const sum = firstNumber + secondNumber;
 
-    const sum = firstNumber + secondNumber;
-
-    res.status(201).json({
-        answer: sum
-    });
-})
+  res.status(201).json({
+    answer: sum,
+  });
+});
 
 /* MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: 'mainframe',
+    dbName: "mainframe",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
